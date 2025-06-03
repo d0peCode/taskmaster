8e5b8855fd1174045bf00e3aa4473dae212ce424
Borys Tymiński <borystyminski@Boryss-MacBook-Pro.local>
2025-06-04 00:11:05 +0200

    feat(task): update Task type

--- src/types/Task.ts
+++ src/types/Task.ts
@@ -1,11 +1,10 @@
-// src/types/Task.ts
 export type TaskStatus = 'pending' | 'in-progress' | 'completed';
 
 export interface Task {
     id: string;
     title: string;
     description: string;
-    dueDate: string; // Or Date, depending on how you want to handle it. String is often simpler for storage.
+    dueDate: string;
     status: TaskStatus;
-    createdAt: Date; // Good to have for sorting or reference
+    createdAt: Date;
 }
